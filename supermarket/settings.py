505ed636d68560bded0d4e7f34a9121fbfbaec59
achujoseph0709 <achujozefsl0709@gmail.com>
2024-08-03 13:10:35 +0530

    static 3

--- supermarket/settings.py
+++ supermarket/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = "supermarket.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),  # Convert to string
     }
 }
+
 
 
 # Password validation
